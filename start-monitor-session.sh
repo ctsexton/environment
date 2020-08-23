65b7b321f1a6086dc88c23e28acbf609e259a267
Cameron <cameron.t.sexton@gmail.com>
2020-08-23 15:29:00 -0700

    refine urxvt defaults and add start-monitor-session script

---
+++ start-monitor-session.sh
@@ -0,0 +1,4 @@
+xrandr --output HDMI-0 --left-of eDP-1-1
+echo "Monitor online - left of default screen"
+xset s off -dpms
+echo "Display power management signalling disabled - screen will not sleep."
